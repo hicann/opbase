a2c63e6e3f454279689af252e36f7487e1a3ff62
liangtongxue <lianglihui@h-partners.com>
2025-09-29 17:16:49 +0800

    head file adapt

--- include/op_common/log/log.h
+++ include/op_common/log/log.h
@@ -4,8 +4,9 @@
  * This file is a part of the CANN Open Software.
  * Licensed under CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
- * See LICENSE in the root of the software repository for the full text of the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. See LICENSE in the root of
+ * the software repository for the full text of the License.
  */
 
 /*!
@@ -22,11 +23,10 @@
 #include <sys/syscall.h>
 #include "exe_graph/runtime/shape.h"
 #include "base/err_msg.h"
-#include "toolchain/dlog_pub.h"
+#include "dlog_pub.h"
 #include "graph/ge_error_codes.h"
 #include "graph/types.h"
 #include "op_common/op_host/util/opbase_export.h"
-#include "toolchain/slog.h"
 namespace Ops {
 namespace Base {
 
