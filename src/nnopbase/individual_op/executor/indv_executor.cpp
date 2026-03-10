0d52b0b658e9e107c2f10b36c8e0dfe08f3d2905
pengjiahao96 <pengjiahao@huawei.com>
2026-03-10 20:09:26 +0800

    修复非连续输入命中连续缓存地址未刷新的问题
    Co-authored-by: pengjiahao96<pengjiahao@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !217 merge 8.5.0 into 8.5.0
    
    修复非连续输入命中连续缓存地址未刷新的问题
    
    Created-by: pengjiahao96
    Commit-by: pengjiahao96
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    修复非连续输入命中连续缓存地址未刷新的问题
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/opbase!217

--- src/nnopbase/individual_op/executor/indv_executor.cpp
+++ src/nnopbase/individual_op/executor/indv_executor.cpp
@@ -874,6 +874,12 @@ static bool NnopbaseExecutorCachedArgs(NnopbaseExecutor *executor)
         // 老流程add io时tensor是添加到ownArgs上，此时也需要更新args上的io地址
         UpdateArgsIoAddr(&executor->args->inputs, &executor->ownArgs.inputs);
         UpdateArgsIoAddr(&executor->args->outputs, &executor->ownArgs.outputs);
+        if (!executor->ownArgs.inputs.unContiguousTensors.idxs.empty()) {
+                executor->args->inputs.unContiguousTensors.idxs = executor->ownArgs.inputs.unContiguousTensors.idxs;
+                executor->args->inputs.unContiguousTensors.workspaceOffsets = executor->ownArgs.inputs.unContiguousTensors.workspaceOffsets;
+                executor->args->inputs.unContiguousTensors.tensors = executor->ownArgs.inputs.unContiguousTensors.tensors;
+                executor->args->inputs.unContiguousTensors.tensorList = executor->ownArgs.inputs.unContiguousTensors.tensorList;
+        }
         // ref算子非连续tensor地址也要刷新
         if (!executor->ownArgs.inputs.unContiguousTensors.refIdxs.empty()) {
             executor->args->inputs.unContiguousTensors.refIdxs = executor->ownArgs.inputs.unContiguousTensors.refIdxs;
