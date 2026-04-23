a5334107575027b4538112f7f0d127e86a4e3518
liu-wei <lovline.liuwei@huawei.com>
2026-04-23 14:04:43 +0800

    aicpu aclnn流程对通用大模型使能
    Co-authored-by: liu-wei<lovline.liuwei@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !332 merge 9.0.0 into 9.0.0
    
    aicpu aclnn流程对通用大模型使能
    
    Created-by: liu-wei
    Commit-by: liu-wei
    Merged-by: cann-robot
    Description: ## 描述
    aicpu aclnn流程支持通用模型的自定义算子流程，对于通用模型omni的aicpu aclnn自定义算子流程支持。
    
    ## 关联的Issue
    https://gitcode.com/cann/opbase/issues/120
    
    ## 测试
    基本功能测试okay，自定义算子功能okay。
    
    ## 文档更新
    不涉及文档更新
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/opbase!332

--- src/nnopbase/aicpu/task_handler/aicpu_json_load_manager.cpp
+++ src/nnopbase/aicpu/task_handler/aicpu_json_load_manager.cpp
@@ -32,7 +32,7 @@ const std::string kAicpuCustOpsFilePath =
 constexpr size_t SOC_VERSION_LEN = 128U;
 constexpr int kMaxFileSizeLimit = INT_MAX;
 const std::string kSplitSeparator = ":";
-const std::set<std::string> kCustomerWhiteList = {"cv", "math", "nn", "transformer"};
+const std::set<std::string> kCustomerWhiteList = {"cv", "math", "nn", "transformer", "omni_custom_ops"};
 }
 
 bool JsonLoadManger::hasAicpuLoadBin_ = false;
@@ -270,12 +270,21 @@ aclnnStatus JsonLoadManger::ParseCustOpInfo() {
   return ACLNN_SUCCESS;
 }
 
-void JsonLoadManger::FillCustOpInfos(const std::string opsRegisterName, const OpInfoDescs &infoDesc) {
+void JsonLoadManger::FillCustOpInfos(std::string opsRegisterName, const OpInfoDescs &infoDesc) {
+  if (!opsRegisterName.empty() && opsRegisterName.back() == '/') {
+    opsRegisterName.pop_back();
+    OP_LOGI("Ops register name: %s", opsRegisterName.c_str());
+  }
+
+  const size_t lastSlash = opsRegisterName.find_last_of('/');
+  const std::string dirName =
+      (lastSlash != std::string::npos) ? opsRegisterName.substr(lastSlash + 1) : opsRegisterName;
+
   const size_t lastUnderscore = opsRegisterName.find_last_of('_');
   if (lastUnderscore != std::string::npos) {
     const std::string suffix = opsRegisterName.substr(lastUnderscore + 1);
-    if (kCustomerWhiteList.count(suffix) == 0U) {
-      OP_LOGI("suffix[%s] is not in customer white list, skip to insert customer ops info. ops register name is %s",
+    if (kCustomerWhiteList.count(suffix) == 0U && dirName != "omni_custom_ops") {
+      OP_LOGI("suffix[%s] is not in customer white list, skip to insert customer ops info. opsRegisterName is %s",
               suffix.c_str(), opsRegisterName.c_str());
       return;
     }
@@ -283,6 +292,7 @@ void JsonLoadManger::FillCustOpInfos(const std::string opsRegisterName, const Op
     OP_LOGW("Failed to extract the sub-repository so suffix, unable to construct the custom so path.");
     return;
   }
+
   // const std::lock_guard<std::mutex> lock(custMutex_);
   for (const auto &opDesc : infoDesc.opInfos) {
     if (opDesc.opType.empty()) {
