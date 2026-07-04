2303597da3afc2b1685a93c80228dcf046420014
cann-robot <cann@cann.team>
2026-07-04 15:09:28 +0800

    revet//refactor: log.h使用toolchain/slog.h替代extern声明，去除dlog_pub.h依赖
    Co-authored-by: cann-robot<cann@cann.team>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !603 merge revert-mr-600-1783148880480-auto into master
    
    revet//refactor: log.h使用toolchain/slog.h替代extern声明，去除dlog_pub.h依赖
    
    Created-by: cann-robot
    Commit-by: cann-robot
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/opbase!603

--- include/op_common/log/log.h
+++ include/op_common/log/log.h
@@ -26,9 +26,25 @@
 #include "graph/types.h"
 #include "op_common/op_host/util/opbase_export.h"
 
-#include "toolchain/slog.h"
+#ifndef DLOG_DEBUG
+#define DLOG_DEBUG 0
+#endif
+#ifndef DLOG_INFO
+#define DLOG_INFO 1
+#endif
+#ifndef DLOG_WARN
+#define DLOG_WARN 2
+#endif
+#ifndef DLOG_ERROR
+#define DLOG_ERROR 3
+#endif
 
 constexpr int32_t OP_MODULE_ID = 63;
+
+extern "C" {
+int32_t CheckLogLevel(int32_t moduleId, int32_t logLevel);
+void DlogRecord(int32_t moduleId, int32_t level, const char* fmt, ...);
+}
 
 namespace Ops {
 namespace Base {
