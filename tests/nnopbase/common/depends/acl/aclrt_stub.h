893ec692ce6be514e309eda8b85b344b415d0dcf
weixin_62388497 <zhouzuhao@h-partners.com>
2026-09-15 14:26:14 +0800

    MC2 KFC场景下将aicpu从流设置为不阻塞，适配LAUNCH BLOCKING场景
    Co-authored-by: weixin_62388497<zhouzuhao@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !847 merge master into master
    
    MC2 KFC场景下将aicpu从流设置为不阻塞，适配LAUNCH BLOCKING场景
    
    Created-by: weixin_62388497
    Commit-by: weixin_62388497
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    https://gitcode.com/cann/opbase/issues/411
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/opbase!847

--- tests/nnopbase/common/depends/acl/aclrt_stub.h
+++ tests/nnopbase/common/depends/acl/aclrt_stub.h
@@ -20,6 +20,8 @@
 
 #include "opdev/op_log.h"
 
+extern int32_t g_aclsysVersionNum;
+
 class AclrtStub {
 public:
     virtual ~AclrtStub() = default;
@@ -85,6 +87,12 @@ public:
                                              aclrtStreamAttrValue* value)
     {
         value->cacheOpInfoSwitch = 1;
+        return ACL_SUCCESS;
+    }
+
+    virtual aclError aclrtSetStreamAttribute(aclrtStream stream, aclrtStreamAttr stmAttrType,
+                                             aclrtStreamAttrValue* value)
+    {
         return ACL_SUCCESS;
     }
 
